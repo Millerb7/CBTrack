8980b9d569c0bee495b880a1252413a54a7ece38
Windmill <millerjboyd@gmail.com>
2021-09-01 13:43:45 -0500

    functionality for login works and logout, error still remains sometimes

--- client/src/components/Navbars/HomeNavbar.js
+++ client/src/components/Navbars/HomeNavbar.js
@@ -1,7 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Auth from '../../utils/auth';
 
 function HomeNavbar() {
+  const logout = (event) => {
+    event.preventDefault();
+    Auth.logout();
+  };
+
   return (
     <nav className="nav flex-row justify-space-around p-4 m-2 bg-dark">
       <ul>
@@ -18,7 +24,26 @@ function HomeNavbar() {
           <Link to="/settings">Settings</Link>
         </li>
         <li>
-          <Link to="/Login">Login</Link>
+          {Auth.loggedIn() ? (
+              <div>
+                <button className="btn btn-lg btn-light m-2" onClick={logout}>
+                  Logout
+                </button>
+              </div>
+            ) : (
+              <div>
+                <div>
+                <Link className="btn btn-lg btn-info m-2" to="/login">
+                  Login
+                </Link>
+                </div>
+                <div>
+                <Link className="btn btn-lg btn-light m-2" to="/signup">
+                  Signup
+                </Link>
+                </div>
+              </div>
+            )}
         </li>
       </ul>
     </nav>
