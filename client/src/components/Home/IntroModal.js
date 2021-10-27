e182ae009004cc7386bf77d61be5b2bd2b95d1a4
Windmill <millerjboyd@gmail.com>
2021-10-27 17:43:07 -0500

    finished current introduction

--- client/src/components/Home/IntroModal.js
+++ client/src/components/Home/IntroModal.js
@@ -2,14 +2,15 @@ import React, { useState } from 'react';
 import PageOne from '../Introduction/PageOne';
 import PageTwo from '../Introduction/PageTwo';
 import PageThree from '../Introduction/PageThree';
+import PageFour from '../Introduction/PageFour';
 
 function IntroModal ({ closeModal }) {
     const [currentPage, setPage] = useState(false);
 
     const getPage = () => {
         if(currentPage <= 0) {
-            setPage(3);
-        } else if(currentPage >= 4) {
+            setPage(4);
+        } else if(currentPage >= 5) {
             setPage(1);
         }
 
@@ -19,6 +20,8 @@ function IntroModal ({ closeModal }) {
             return <PageTwo />;
         } else if(currentPage === 3) {
             return <PageThree />;
+        } else if(currentPage === 4) {
+            return <PageFour />;
         }
     }
 
@@ -35,9 +38,10 @@ function IntroModal ({ closeModal }) {
                         <div className="column is-1 is-flex is-align-items-center" onClick={() => setPage(currentPage+1)}><i className="fas fa-chevron-right"></i></div>
                     </div>
                     <div className="has-text-centered">
-                        {currentPage===1 ? (<i class="fas fa-circle p-1"></i>) : (<i class="far fa-circle p-1"></i>)}
-                        {currentPage===2 ? (<i class="fas fa-circle p-1"></i>) : (<i class="far fa-circle p-1"></i>)}
-                        {currentPage===3 ? (<i class="fas fa-circle p-1"></i>) : (<i class="far fa-circle p-1"></i>)}
+                        {currentPage===1 ? (<i className="fas fa-circle p-1" onClick={() => setPage(1)}></i>) : (<i className="far fa-circle p-1" onClick={() => setPage(1)}></i>)}
+                        {currentPage===2 ? (<i className="fas fa-circle p-1" onClick={() => setPage(2)}></i>) : (<i className="far fa-circle p-1" onClick={() => setPage(2)}></i>)}
+                        {currentPage===3 ? (<i className="fas fa-circle p-1" onClick={() => setPage(3)}></i>) : (<i className="far fa-circle p-1" onClick={() => setPage(3)}></i>)}
+                        {currentPage===4 ? (<i className="fas fa-circle p-1" onClick={() => setPage(4)}></i>) : (<i className="far fa-circle p-1" onClick={() => setPage(4)}></i>)}
                     </div>
                 </section>
                 
--- client/src/components/Introduction/PageFour.js
+++ client/src/components/Introduction/PageFour.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
-function PageThree () {
+function PageFour () {
     return (
         <div className="column is-10">
             <h1 className="title">How to Self CBT</h1>
-            <p className="p-2">The main way to succeed in CBT is to change your thoughts, so this application allows for you to enter your distortions along with a more valid statement.</p>
-            <p className="p-2"></p>
+            <p className="p-2">The main way to succeed in CBT is to change your thoughts, so this application allows for you to enter your distortions along with a corrected statement.</p>
+            <p className="p-2">If you were to have a distortion of "I mess up everything I do" Its important to figure out why you think that. Noting all of the possible causes will allow you to figure out what is causing the self destructive thoughts and put an end to them.</p>
+            <p className="p-2">At the bottom of most pages you will see a text field to enter all cognitive distortions you come across. Next to this field will be another field to list any other identifying information you wish. All of the inofrmation will then be stored together.</p>
+            <p className="p-2">If you go to the log page, you can view every thought you have submitted along with the extra information. This will be sorted by date, however if you want to narrow the field of thoughts by date go to the calendar page.</p>
         </div>
     );
 };
 
-export default PageThree;
+export default PageFour;
